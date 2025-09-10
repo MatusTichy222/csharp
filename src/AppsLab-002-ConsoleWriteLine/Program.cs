66515d90c3c2b66fbbd78a478c187c1b479c9676
tichym25 <tichymatus3@gmail.com>
2025-09-10 08:54:11 +0200

    zmena vypisovania na konzoli

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//vypise na konzoli text
+Console.WriteLine("Hello, Matúš!");
+//Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+
+
+
+//zakomentovat medzi lomitko a hviezdicka
+/*Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");
+Console.WriteLine("Hello, Matúš!");*/
+
+
