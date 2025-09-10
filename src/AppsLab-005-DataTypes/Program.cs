ba3cfafdeb55469a47e13fb157a4d8c1665d422d
tichym25 <tichymatus3@gmail.com>
2025-09-10 10:06:59 +0200

    bool, int, float, string, char skusanie + poznamky

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -7,19 +7,23 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
+            //true/false
             bool isAdult = false;
             Console.WriteLine(isAdult);
             
+            // cislo
             int myAge = 15;
             Console.WriteLine(myAge);
 
+            //desatinne cislo
             float pi = 3.14f;
             Console.WriteLine(pi);
 
-
+            //viac znakov hociakych
             string name = "Matus";
             Console.WriteLine(name);
 
+            //jeden znak
             char gender = 'm';
             Console.WriteLine(gender);
 
