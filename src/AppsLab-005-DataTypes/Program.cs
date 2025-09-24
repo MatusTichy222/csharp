8c4adeb18018b2ba0cd15f805a58b18289aee609
tichym25 <tichymatus3@gmail.com>
2025-09-24 13:09:21 +0200

    kalkulacka a pisanie do konzoly

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata;
+using System.Threading.Channels;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace MyApp
@@ -70,59 +71,155 @@ namespace MyApp
             int birthMonth = 7;
             int birthYear = 2010;
 
-/*
-            //scitanie datumu mesiaca a roku narodenia
-            int birthSum = birthDay + birthMonth + birthYear;
-            //vypisanie na konzol
-            Console.WriteLine(birthSum);
+            /*
+                        //scitanie datumu mesiaca a roku narodenia
+                        int birthSum = birthDay + birthMonth + birthYear;
+                        //vypisanie na konzol
+                        Console.WriteLine(birthSum);
 
 
-            //vynasobenie birthSum x 10 a vypisat na kozolu
-            Console.WriteLine(birthSum * 10);
+                        //vynasobenie birthSum x 10 a vypisat na kozolu
+                        Console.WriteLine(birthSum * 10);
 
-            //scitat datum a mesiac narodenia a prenasobit rokom
-            int birthSum2 = birthDay + birthMonth;
-            Console.WriteLine(birthSum2 * birthYear);
-            // druha moznost
-            Console.WriteLine((birthMonth + birthDay) * birthYear);
-
-
-            //vzdy to zaokruhkuje
-            //vzdy float (3f) ked chces desatinne cisla
-            Console.WriteLine(5 / 3);
-            Console.WriteLine(5 / 3f);
-
-            string Name2 = "Matus";
-            Console.WriteLine(Name2 + birthYear);
-
-            //vypisat meno a sucet dna a mesiaca narodenia
-
-            Console.WriteLine("Matus" + (birthDay + birthYear));*/
+                        //scitat datum a mesiac narodenia a prenasobit rokom
+                        int birthSum2 = birthDay + birthMonth;
+                        Console.WriteLine(birthSum2 * birthYear);
+                        // druha moznost
+                        Console.WriteLine((birthMonth + birthDay) * birthYear);
 
 
-            bool result = 6 != 3;
-            Console.WriteLine(result);
+                        //vzdy to zaokruhkuje
+                        //vzdy float (3f) ked chces desatinne cisla
+                        Console.WriteLine(5 / 3);
+                        Console.WriteLine(5 / 3f);
 
-            bool result2 = 7 == 3;
-            bool result3 = 3 < 10;
+                        string Name2 = "Matus";
+                        Console.WriteLine(Name2 + birthYear);
 
-            string Igor = "Igor";
-            string Michal = "Michal";
-            Console.WriteLine(Igor == Michal);
+                        //vypisat meno a sucet dna a mesiaca narodenia
 
-            string igor = "Igor";
-            string michal = igor;
-            Console.WriteLine(Igor == Michal);
+                        Console.WriteLine("Matus" + (birthDay + birthYear));*/
 
 
-            string krabickaA = "Igor";
-            string krabickaB = krabickaA;
-            krabickaA = "Michal";
-            Console.WriteLine(krabickaB == krabickaA);
+            /* bool result = 6 != 3;
+             Console.WriteLine(result);
+
+             bool result2 = 7 == 3;
+             bool result3 = 3 < 10;
+
+             string Igor = "Igor";
+             string Michal = "Michal";
+             Console.WriteLine(Igor == Michal);
+
+             string igor = "Igor";
+             string michal = igor;
+             Console.WriteLine(Igor == Michal);
+
+
+             string krabickaA = "Igor";
+             string krabickaB = krabickaA;
+             krabickaA = "Michal";
+             Console.WriteLine(krabickaB == krabickaA); */
+
+
+            /*
+           Console.WriteLine("Napis meno");
+           string meno = Console.ReadLine();
+           string vypis = "Ahoj ";
+           Console.WriteLine(vypis + meno);
+
+           Console.WriteLine("Zadaj vek: ");
+
+
+           string vekText = Console.ReadLine();
+           int vek = int.Parse(vekText);
+           int birthYear = 2025 - vek;
+           string vypis2 = "Ahoj " + meno + " narodil si sa v roku " + birthYear;
+           Console.WriteLine(vypis2);
+
+
+
+           if (birthYear > 2000)
+           {
+               Console.WriteLine("Si narodeny po roku 2000");
+           }
+
+           else if (birthYear == 2000)
+
+           {
+               Console.WriteLine("Si narodeny v roku 2000");
+           }
 
 
 
 
+
+
+
+           else
+           {
+               Console.WriteLine("Si narodeny pred rokom 2000");
+           }
+           */
+
+
+            /* Console.WriteLine("Zadaj znamku z matematiky");
+             string znamkamText = Console.ReadLine();
+             int znamkam = int.Parse(znamkamText);
+
+             Console.WriteLine("Teraz zadaj znamku zo slovenciny");
+             string znamkasText = Console.ReadLine();
+             int znamkas = int.Parse(znamkasText);
+             if (znamkam <= 2 && znamkas <= 2)
+             {
+                 Console.WriteLine("Tvoj priemer je dostacujuci");
+             }
+             else
+             {
+                 Console.WriteLine("Tvoj priemer je nedostacujuci");
+             }
+            */
+
+
+
+            Console.WriteLine("Zadaj prve cislo");
+            string cislo1text = Console.ReadLine();
+            int cislo1 = int.Parse(cislo1text);
+
+
+            Console.WriteLine("Zadaj poctovu operaciu");
+            string operacia = Console.ReadLine();
+
+
+            Console.WriteLine("Zadaj druhe cislo");
+            string cislo2text = Console.ReadLine();
+            int cislo2 = int.Parse(cislo2text);
+
+            int vysledok = 0;
+
+            if (operacia == "+")
+            {
+                vysledok = cislo1 + cislo2;
+            }
+
+            else if (operacia == "-")
+            {
+                vysledok = cislo1 - cislo2;
+            }
+            else if (operacia == "*")
+            {
+                vysledok = cislo1 * cislo2;
+            }
+            else if (operacia == "" + "/")
+            {
+                vysledok = cislo1 / cislo2;
+            }
+
+            Console.WriteLine("vysledok je: " + vysledok);
+
+
+            
+                
         }
     }
 }
