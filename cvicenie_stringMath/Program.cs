9fb3dce5f460d9d9c11189ff78920b82f522d994
tichym25 <tichymatus3@gmail.com>
2025-10-15 12:25:43 +0200

    cvicenie matika a subor

---
+++ cvicenie_stringMath/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace cvicenie_stringMath
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           /* Console.WriteLine("zadaj cislo");
+            string numberT = Console.ReadLine();
+            int number = int.Parse(numberT);
+            Console.WriteLine("zadaj cislo 2");
+            string number2T = Console.ReadLine();
+            int number2 = int.Parse(number2T);
+            int sum = Scitanie(1, 2, 3, number, number2);
+            Console.WriteLine(sum);*/
+        }
+         
+
+       /* public static int Scitanie(int a , int b, int c, int d, int e)
+        { 
+        
+            int f = a + b + c + d + e;
+            return f;
+        }*/
+
+        public static void RodneCislo(string[] text)
+        {
+            
+            foreach (string line in text.Skip(1))
+            {
+                string[] splitsT = line.Split(";");
+                int splits = int.Parse(splitsT[2]);
+                Console.WriteLine(splits);
+
+            }
+
+
+        
+
+
+
+        }
+}
---
+++ cvicenie_subor/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace cvicenie_subor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ;
+            string[] text = File.ReadAllLines("People_100.csv");
+            // MinMoneyCountAvg(text);
+            UndHalfMilion(text);
+            // RodneCislo(text);
+            /*MoneyCountAvg(text);
+            MoneyCountAvg(text);
+            MoneyCountAvg(text);
+            MoneyCountAvg(text);
+            MoneyCountAvg(text);
+            MoneyCountAvg(text);
+           */
+        }
+
+
+        /* public static void MoneyCountAvg(string[] text)
+     {
+         int sum = 0;
+         foreach (string line in text.Skip(1))
+         {
+             string[] splitsT = line.Split(";");
+             int splits = int.Parse(splitsT[4]);
+             sum += splits;
+
+         }
+
+
+         Console.WriteLine(sum / (text.Count() - 1));*/
+
+
+        /*  public static void RodneCislo(string[] text)
+          {
+
+              foreach (string line in text.Skip(1))
+              {
+                  string[] splitsT = line.Split(";");
+
+                  Console.WriteLine(splitsT[2]);
+
+              }
+
+          }*/
+
+        /*  public static void MinMoneyCountAvg(string[] text)
+          {
+              int minValue = 99999999;
+              string minValuePerson = "";
+              foreach (string line in text.Skip(1))
+              {
+                  string[] splitsT = line.Split(";");
+                  int accountValue = int.Parse(splitsT[4]);
+                  if (accountValue < minValue)
+                  {
+                      minValue = accountValue;
+                      minValuePerson = splitsT[0] + " " + splitsT[1];
+                  }
+
+              }
+
+
+              Console.WriteLine(minValuePerson);*/
+
+        //vsetkych ktory maju pod pol miliona na ucte
+        //posleme tam text a ona nam musi vratit polie vsetkych mien
+
+
+        public static void UndHalfMilion(string[] text)
+        {
+            int minValue = 500000;
+            string minValuePerson = "";
+            foreach (string line in text.Skip(1))
+            {
+                string[] splitsT = line.Split(";");
+                int accountValue = int.Parse(splitsT[4]);
+                if (accountValue < minValue)
+                {
+                    minValue = accountValue;
+                    minValuePerson = splitsT[0] + " " + splitsT[1];
+                    Console.WriteLine(minValuePerson);
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
