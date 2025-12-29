863bb72c6e28bbe3802ee1b2bc5219760356d37a
tichy <tichymatus3@gmail.com>
2025-12-29 11:52:59 +0100

    praca na projekte 28.12

--- Casino/Casino.cs
+++ Casino/Casino.cs
@@ -13,11 +13,33 @@ namespace Casino
 
         public void StartCasino()
         {
-            Console.WriteLine("Zadaj svoje Meno");
+            LogoCasino logoCSN = new LogoCasino();
+            logoCSN.StartUI();
+            //Thread.Sleep(500);
+            Console.Clear();
+
+           
+            
+            Console.WriteLine(@" ====================================
+ |        CASINO ADMIRAL LOGIN       |
+ ====================================
+ |                                  |
+ |   Zadaj meno:                    |
+ |                                  |
+ |   Zadaj vek :                    |
+ |                                  |
+ |        [   P O K R A Č O V A Ť   ] |
+ ====================================");
+            Console.SetCursorPosition(18, 4);
             string name = Console.ReadLine();
-            Console.WriteLine("Vitaj " + name + " zadaj svoj vek");
+
+            Console.SetCursorPosition(18, 6);
             string ageTxt = Console.ReadLine();
             int age = int.Parse(ageTxt);
+            Console.Clear();
+            Console.WriteLine("LOADING.......");
+            Thread.Sleep(500);
+
 
             if (age < 18)
             {
@@ -79,11 +101,11 @@ namespace Casino
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine(@"   _____ _       _     __  __            _     _             
-  / ____| |     | |   |  \/  |          | |   (_)            
- | (___ | | ___ | |_  | \  / | __ _  ___| |__  _ _ __   ___  
-  \___ \| |/ _ \| __| | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \ 
-  ____) | | (_) | |_  | |  | | (_| | (__| | | | | | | |  __/ 
- |_____/|_|\___/ \__| |_|  |_|\__,_|\___|_| |_|_|_| |_|\___| 
+  / ____| |     | |    |  \/  |          | |   (_)            
+ | (___ | | ___ | |_   | \  / | __ _  ___| |__  _ _ __   ___  
+  \___ \| |/ _ \| __|  | |\/| |/ _` |/ __| '_ \| | '_ \ / _ \ 
+  ____) | | (_) | |_   | |  | | (_| | (__| | | | | | | |  __/ 
+ |_____/|_|\___/ \__|  |_|  |_|\__,_|\___|_| |_|_|_| |_|\___| 
                                                              
                                                              ");
                             slotMachine.SlotMachineGame(MyPlayer);
@@ -93,17 +115,27 @@ namespace Casino
                             Console.Clear();
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine(@"  _    _           _   __  __ _                       
- | |  | |         | | |  \/  (_)                      
- | |__| | ___   __| | | \  / |_ _ __   ___ ___  _   _ 
- |  __  |/ _ \ / _` | | |\/| | | '_ \ / __/ _ \| | | |
- | |  | | (_) | (_| | | |  | | | | | | (_| (_) | |_| |
- |_|  |_|\___/ \__,_| |_|  |_|_|_| |_|\___\___/ \__,_|
+ | |  | |         | |  |  \/  (_)                      
+ | |__| | ___   __| |  | \  / |_ _ __   ___ ___  _   _ 
+ |  __  |/ _ \ / _` |  | |\/| | | '_ \ / __/ _ \| | | |
+ | |  | | (_) | (_| |  | |  | | | | | | (_| (_) | |_| |
+ |_|  |_|\___/ \__,_|  |_|  |_|_|_| |_|\___\___/ \__,_|
                                                       
                                                       ");
                             Console.ResetColor();
                             hodMincou.HodMincouGame(MyPlayer);
                             break;
                         case 9:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(@"  _____             _     _            _               _     _ _                                          _               _                
+ |  __ \           (_)   | |          (_)             (_)   | (_)                                        | |             | |               
+ | |  | | _____   ___  __| | ___ _ __  _  __ _  __   ___  __| |_ _ __ ___   ___   ___  __ _   _ __   __ _| |__  _   _  __| |_   _  ___ ___ 
+ | |  | |/ _ \ \ / / |/ _` |/ _ \ '_ \| |/ _` | \ \ / / |/ _` | | '_ ` _ \ / _ \ / __|/ _` | | '_ \ / _` | '_ \| | | |/ _` | | | |/ __/ _ \
+ | |__| | (_) \ V /| | (_| |  __/ | | | | (_| |  \ V /| | (_| | | | | | | |  __/ \__ \ (_| | | | | | (_| | |_) | |_| | (_| | |_| | (_|  __/
+ |_____/ \___/ \_/ |_|\__,_|\___|_| |_|_|\__,_|   \_/ |_|\__,_|_|_| |_| |_|\___| |___/\__,_| |_| |_|\__,_|_.__/ \__,_|\__,_|\__,_|\___\___|
+                                                                                                                                           
+                                                                                                                                           ");
+                            Console.ResetColor();
                             return;
                         default:
                             Console.ForegroundColor = ConsoleColor.Red;
---
+++ test casino/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Channels;
+
+namespace test_casino
+{
+    internal class Program
+    {
+        public class LogoCasino
+        {
+            private void LogoCSN()
+            {
+                string[] lines = new string[]
+                {
+" ██████╗ █████╗ ███████╗██╗███╗   ██╗ ██████╗      █████╗ ██████╗ ███╗   ███╗██╗██████╗  █████╗ ██╗     ",
+"██╔════╝██╔══██╗██╔════╝██║████╗  ██║██╔═══██╗    ██╔══██╗██╔══██╗████╗ ████║██║██╔══██╗██╔══██╗██║     ",
+"██║     ███████║███████╗██║██╔██╗ ██║██║   ██║    ███████║██║  ██║██╔████╔██║██║██████╔╝███████║██║     ",
+"██║     ██╔══██║╚════██║██║██║╚██╗██║██║   ██║    ██╔══██║██║  ██║██║╚██╔╝██║██║██╔══██╗██╔══██║██║     ",
+"╚██████╗██║  ██║███████║██║██║ ╚████║╚██████╔╝    ██║  ██║██████╔╝██║ ╚═╝ ██║██║██║  ██║██║  ██║███████╗",
+" ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝"
+
+                };
+
+                int startY = (Console.WindowHeight / 2) - (lines.Length / 2) - 2;
+                int widthConsole = Console.WindowWidth;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string line = lines[i];
+                    int xStart = (widthConsole / 2) - (line.Length / 2);
+                    if (xStart < 0) xStart = 0;
+                    int y = startY + i;
+                    if (y < 0) y = 0;
+                    try
+                    {
+                        Console.SetCursorPosition(xStart, y);
+                        Console.WriteLine(line);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Error!!");
+                    }
+                }
+            }
+        }
+    }
+}
